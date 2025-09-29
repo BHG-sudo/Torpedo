3b5dce5f07a1da5f6aeee7d6c4cf383151ae72fa
BHG-sudo <balazshgeza@gmail.com>
2025-09-29 11:29:13 +0200

    Add files via upload

--- Torpedo.py
+++ Torpedo.py
@@ -1,5 +1,18 @@
 from tkinter import *
 from tkinter import ttk
+import time
+
+
+def nyertel():
+    root = Tk()
+    root.title("Torpedo")
+    root.geometry("1100x160")
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Gratulálok nyertél!", font=("calibre", 100, "normal")).grid(column=1, row=1)
+    root.update()
+    time.sleep(5)
+    root.destroy()  
 
 def palya_generalas(palya_dict: dict, hajo_dict: dict):
     palya = []
@@ -9,16 +22,20 @@ def palya_generalas(palya_dict: dict, hajo_dict: dict):
         if state == 0 and hajo_dict.get(cell) == 1:
             palya.append("0")
         if state == 1 and hajo_dict.get(cell) == 1:
-            palya.append("X")
-        if state == 1 and hajo_dict.get(cell) == 0:
             palya.append("#")
+        if state == 1 and hajo_dict.get(cell) == 0:
+            palya.append("X")
     return palya
 
 def tamad():
     tamad = tamad_var.get()
     palya_dict[tamad] = 1
     palya = palya_generalas(palya_dict, hajo_dict)
-                
+    kilott_szam = palya.count("#")
+    if kilott_szam == 17:
+        nyertel()
+        root.destroy()
+    
     counter = 0
     for i in range(10):
         ttk.Label(frm, text=label[i]).grid(column=i+1, row=0)
@@ -28,6 +45,7 @@ def tamad():
             counter += 1
 
     root.update()
+
 
 label = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
@@ -42,16 +60,16 @@ palya_dict = {"A1": 0, "B1": 0, "C1": 0, "D1": 0, "E1": 0, "F1": 0, "G1": 0, "H1
               "A9": 0, "B9": 0, "C9": 0, "D9": 0, "E9": 0, "F9": 0, "G9": 0, "H9": 0, "I9": 0, "J9": 0,
               "A10": 0, "B10": 0, "C10": 0, "D10": 0, "E10": 0, "F10": 0, "G10": 0, "H10": 0, "I10": 0, "J10": 0}
 
-hajo_dict = {"A1": 0, "B1": 0, "C1": 0, "D1": 0, "E1": 0, "F1": 0, "G1": 0, "H1": 0, "I1": 0, "J1": 0,
+hajo_dict = {"A1": 0, "B1": 0, "C1": 0, "D1": 0, "E1": 0, "F1": 0, "G1": 0, "H1": 1, "I1": 1, "J1": 1,
              "A2": 0, "B2": 0, "C2": 0, "D2": 0, "E2": 0, "F2": 0, "G2": 0, "H2": 0, "I2": 0, "J2": 0,
-             "A3": 0, "B3": 0, "C3": 0, "D3": 0, "E3": 0, "F3": 0, "G3": 0, "H3": 0, "I3": 0, "J3": 0,
-             "A4": 0, "B4": 0, "C4": 0, "D4": 0, "E4": 0, "F4": 0, "G4": 0, "H4": 0, "I4": 0, "J4": 0,
-             "A5": 0, "B5": 0, "C5": 0, "D5": 0, "E5": 0, "F5": 0, "G5": 0, "H5": 0, "I5": 0, "J5": 0,
-             "A6": 0, "B6": 0, "C6": 0, "D6": 0, "E6": 0, "F6": 0, "G6": 0, "H6": 0, "I6": 0, "J6": 0,
-             "A7": 0, "B7": 0, "C7": 0, "D7": 0, "E7": 0, "F7": 0, "G7": 0, "H7": 0, "I7": 0, "J7": 0,
-             "A8": 0, "B8": 0, "C8": 0, "D8": 0, "E8": 0, "F8": 0, "G8": 0, "H8": 0, "I8": 0, "J8": 0,
+             "A3": 1, "B3": 1, "C3": 1, "D3": 0, "E3": 0, "F3": 0, "G3": 0, "H3": 0, "I3": 0, "J3": 0,
+             "A4": 0, "B4": 0, "C4": 0, "D4": 0, "E4": 0, "F4": 0, "G4": 1, "H4": 0, "I4": 0, "J4": 0,
+             "A5": 0, "B5": 0, "C5": 0, "D5": 0, "E5": 0, "F5": 0, "G5": 1, "H5": 0, "I5": 0, "J5": 0,
+             "A6": 0, "B6": 0, "C6": 0, "D6": 0, "E6": 0, "F6": 0, "G6": 1, "H6": 0, "I6": 0, "J6": 0,
+             "A7": 0, "B7": 0, "C7": 0, "D7": 1, "E7": 0, "F7": 0, "G7": 1, "H7": 0, "I7": 0, "J7": 0,
+             "A8": 0, "B8": 0, "C8": 0, "D8": 1, "E8": 0, "F8": 0, "G8": 1, "H8": 0, "I8": 0, "J8": 0,
              "A9": 0, "B9": 0, "C9": 0, "D9": 0, "E9": 0, "F9": 0, "G9": 0, "H9": 0, "I9": 0, "J9": 0,
-             "A10": 0, "B10": 0, "C10": 0, "D10": 0, "E10": 0, "F10": 0, "G10": 0, "H10": 0, "I10": 0, "J10": 0}
+             "A10": 1, "B10": 1, "C10": 1, "D10": 1, "E10": 0, "F10": 0, "G10": 0, "H10": 0, "I10": 0, "J10": 0}
 
 
 
